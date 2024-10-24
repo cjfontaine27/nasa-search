1cbe2d2f0ceb6a08f1d326c5d9411ef094bd8493
cjfontaine27 <cjfontaine11@gmail.com>
2024-10-24 12:48:40 -0400

    css nasa-search

--- nasa-search.js
+++ nasa-search.js
@@ -38,22 +38,44 @@ export class nasaSearch extends DDDSuper(I18NMixin(LitElement)) {
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
+    return css`
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+      :host([loading]) .results {
+        opacity: 0.1;
+        visibility: hidden;
+        height: 1px;
       }
-      h3 span {
-        font-size: var(--nasa-search-label-font-size, var(--ddd-font-size-s));
+      .results {
+        visibility: visible;
+        height: 100%;
+        opacity: 1;
+        transition-delay: .5s;
+        transition: .5s all ease-in-out;
       }
-    `];
+
+      details {
+        margin: 16px;
+        padding: 16px;
+        background-color: blue;
+      }
+      summary {
+        font-size: 24px;
+        padding: 8px;
+        color: white;
+        font-size: 42px;
+      }
+      input {
+        font-size: 20px;
+        line-height: 40px;
+        width: 100%;
+      }
+    `;
+  }
+
+  somerandommethod(e){
+    window.open("whatever.com", "blank");
   }
 
   updated(changedProperties) {
